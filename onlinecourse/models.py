c26c99ea22f9573b01302acb667a115baa9186d1
Matvey Me Myself <veron.vbn1234@gmail.com>
2023-05-29 08:07:35 -0400

    Getting views done

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -97,33 +97,13 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-""" # <HINT> Create a Question Model with:
+# <HINT> Create a Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-class Question(models.Model):
-    courses = models.ManyToManyField(Course)
-    # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=False)
-    # question text
-    text = models.CharField(
-        max_length = 500,
-        default = "This is a sample question.")
-    # question grade/mark
-    grade_marks = models.FloatField(default=1.0)
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    def is_get_score(self, selected_ids):
-        all_answers = self.choice_set.filter(is_correct=True).count()
-        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
-            return True
-        else:
-            return False
 
-    def __str__(self):
-        return self.question_text """
 
 
 class Question(models.Model):
